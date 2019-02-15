28f46dc000db29972fd4c951ab3dc2d845ff19b3
huashengdong <297208097@qq.com>
2019-02-15 11:20:42 +0800

    map_location

---
+++ map_location/pages/map/index.js
@@ -0,0 +1,39 @@
+//index.js
+//获取应用实例
+const app = getApp()
+const map = require('../../utils/qqmap-wx-jssdk.min.js');
+
+let qqmapsdk;
+
+Page({
+  data: {
+  },
+  onLoad: function () {
+
+    let _this = this;
+
+    // 获取用户地理位置
+    wx.getLocation({
+      type: 'gcj02', // gcj02 反应在小程序地图上；wgs84 返回的 gps 坐标
+      success(res) {
+
+        const latitude = res.latitude
+        const longitude = res.longitude
+        const speed = res.speed
+        const accuracy = res.accuracy
+
+        _this.setData({
+          latitude: latitude,
+          longitude: longitude,
+          markers: [{
+            id: "0",
+            latitude: latitude,
+            longitude: longitude,
+            title: "marker",
+          }]
+        });
+
+      }
+    })
+  },
+})
